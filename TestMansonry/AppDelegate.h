bf62fa3ae7a5d835966ae11e969f6aca4327afde
guchengfeng <554430962@qq.com>
2016-08-31 14:54:36 +0800

    Initial Commit

---
+++ TestMansonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestMansonry
+//
+//  Created by OPS on 16/8/31.
+//  Copyright © 2016年 OPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestMansonry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestMansonry
+//
+//  Created by OPS on 16/8/31.
+//  Copyright © 2016年 OPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
